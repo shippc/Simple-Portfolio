5121405763164dc0783eaea2cc2e25a82a72dd29
shippc <wilhanmax@hotmail.com>
2022-08-13 17:55:47 -0300

    ScrollReveal Animation

--- js/main.js
+++ js/main.js
@@ -52,4 +52,18 @@ themeButton.addEventListener('click', () => {
 
 
 /* SCROLL REVEL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
 
+sr.reveal(`.profile-border`)
+sr.reveal(`.profile-name`, {delay:500})
+sr.reveal(`.profile-profession`, {delay:600})
+sr.reveal(`.profile-social`, {delay:700})
+sr.reveal(`.profile-info-group`, {interval:100, delay:700})
+sr.reveal(`.profile-buttons`, {delay:800})
+sr.reveal(`.filters-content`, {delay:900})
+sr.reveal(`.filters`, {delay:1000})
